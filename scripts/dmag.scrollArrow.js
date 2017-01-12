d807c032b643f75459e6c1785c9c4426e2096ea1
Christina Rees <crees@risd.edu>
2017-01-12 15:30:50 -0600

    scroll arrow on jump links

--- scripts/dmag.scrollArrow.js
+++ scripts/dmag.scrollArrow.js
@@ -1,25 +1,29 @@
 (function($) {
 
+	DMAG.$spaceTop = DMAG.isMediumScreen() ? 10 : DMAG.$nav_height + 10;
+
 	DMAG.scrollArrow = {
 		init: function() {
 			$('.js-scrollArrow').click(function() {
 				$('html, body').animate({
-					scrollTop: $('.js-scrollOffset').offset().top
+					scrollTop: $('.js-scrollOffset').offset().top - DMAG.$spaceTop
 				}, 900);
 			});
+
+
 		}
 	};
 
 	DMAG.scrollToc = {
 		init: function() {
-			$spaceTop = DMAG.isMediumScreen() ? 10 : DMAG.$nav_height + 10;
 			$(".js-scrollToc a").click(function(e) {
 				e.preventDefault();
 				$('html, body').animate({
-					scrollTop: $("."+$(this).data("scrollto")).offset().top - $spaceTop
+					scrollTop: $("."+$(this).data("scrollto")).offset().top - DMAG.$spaceTop
 				}, 900);
+				$('.js-scrollArrow').css('display', 'block');
 			});
-		}
+		},
 	};
 
 })(jQuery);
