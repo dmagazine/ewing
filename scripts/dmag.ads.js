aa52d542c682e305f1dd52df2f7cedbef3c92f4f
Christina Rees <crees@risd.edu>
2018-04-03 10:40:11 -0500

    clean up ad file

--- scripts/dmag.ads.js
+++ scripts/dmag.ads.js
@@ -479,7 +479,7 @@
 			if ($('.js-stickyStop').length > 0) {
 				var $padding = 10;
 				var $marginTop = DMAG.$nav_height + $padding;
-				var $sidebarAd = $('.layout--with-sidebar .js-sticky:not(.scroll-to-fixed-fixed)');
+				var $sidebarAd = $('.js-sticky:not(.scroll-to-fixed-fixed)');
 
 				$sidebarAd.each(function(){
 					$this = $(this);
@@ -495,17 +495,6 @@
 							// limit is calculated from the top of ad element
 							// translation: (widget offset) - (ad height) - (padding)
 							limit: $limit,
-							// removes left and top position added by plugin while sticky, 
-							// once sidebar is back to absolute position
-							removeOffsets: true,
-							// becasue ad is below another element in the sidebar (sidebar widget), 
-							// have to account for that element's height
-							preAbsolute: function() {
-								$this.css('marginTop', $this.siblings('.js-stickySibling').height() + 'px');
-							},
-							postAbsolute: function() {
-								$this.css('marginTop', 0);
-							}
 						}); //scrolltofixed
 					} //endif
 
